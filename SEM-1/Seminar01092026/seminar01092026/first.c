74dbd312c1e1c0f7bdabefd18cfdafb076cad307
Andrey-Zobnin <andy.zobnin@gmail.com>
2026-09-01 11:31:23 +0300

    кодификатор формата

--- SEM-1/Seminar01092026/seminar01092026/first.c
+++ SEM-1/Seminar01092026/seminar01092026/first.c
@@ -11,11 +11,11 @@ int main()
 {
     float a = 245.1;
     // printf("Hello, world! \n"); // что такое комментарий
-    printf("a = %i\n", a);
-    printf("a = %u\n", a);
-    printf("a = %u\n", a);
-    printf("a = %u\n", a);
-    printf("a = %d\n", a);
+    // printf("a = %i\n", a);
+    // printf("a = %u\n", a);
+    // printf("a = %u\n", a);
+    // printf("a = %u\n", a);
+    printf("a = %.2f\n", a);
     return 0; 
     /*
     Большой комментарий в несколько строк
