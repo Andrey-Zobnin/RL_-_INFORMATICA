17d5716e5fb3f2c9c20b37bae3718dac6fcb7950
Andrey-Zobnin <andy.zobnin@gmail.com>
2026-09-03 10:37:13 +0300

    задача с радиусом

--- LAB03092026/task1.c
+++ LAB03092026/task1.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include <cstdio>
 
-// const double pi = 3.14;
-// global
-
-int main()
-{
-    const double pi = 3.14;
+int main() {
+    const double pi = 3.14159;
     int r = 0;
-    float pr, sq;
-    printf("");
+    double pr, sq;  // double вместо float
 
+    printf("Enter radius: ");
+    scanf("%d", &r);  // %d для int
+    pr = 2 * pi * r;
+    sq = pi * r * r;
+    printf("Perimeter = %.2f\n", pr);
+    printf("Square = %.2f\n", sq);
+    return 0;
 }
